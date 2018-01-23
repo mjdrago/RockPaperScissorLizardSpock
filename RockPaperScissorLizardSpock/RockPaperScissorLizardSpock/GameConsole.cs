c064a335689d6fc846baae283aef706b594e2f4f
mjdrago <mario.j.drago@gmail.com>
2018-01-22 23:56:43 -0600

    Created two classes - one for the game interactions (GameConsole) and another one for the people playing the game (Player). Also created constructor for the GameConsole differentiating betweena 1 player and 2 player game.

---
+++ RockPaperScissorLizardSpock/RockPaperScissorLizardSpock/GameConsole.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorLizardSpock
+{
+    class GameConsole
+    {
+        public Player player1;
+        public Player player2;
+        public GameConsole()
+        {
+            Console.WriteLine("Is this a two-player or one-player game? \n" +
+                              "Please enter '1' for one-player game or '2' for a two-player game.");
+            char selection = char.Parse(Console.ReadLine());
+
+            switch (selection)
+            {
+                case '1':
+                    player1 = new Player("yes");
+                    player2 = new Player("no");
+                    break;
+                case '2':
+                    player1 = new Player("yes");
+                    player2 = new Player("yes");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice selected. Please try again");
+                    break;
+            }
+        }
+    }
+}
---
+++ RockPaperScissorLizardSpock/RockPaperScissorLizardSpock/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorLizardSpock
+{
+    class Player
+    {
+        string isHuman;
+        string currentSelection;
+        string[] roundResults = new string[3];
+
+        public Player(string isHuman)
+        {
+            this.isHuman = isHuman;
+        }
+    }
+}
